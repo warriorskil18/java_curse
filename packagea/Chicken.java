8e418be5308994cbce5ff81f05d8dc10fad0952b
warriorskil18 <warriorskil18@gmail.com>
2026-04-25 11:07:44 -0600

    Primer commit

---
+++ packagea/Chicken.java
@@ -0,0 +1,12 @@
+package packagea;
+
+public class Chicken {
+	
+	int numEggs = 0; //Inicializamos en linea
+	String name;
+	
+	public Chicken() {
+		name = "Duke"; //Inicializamos el constructor
+	}
+
+}
