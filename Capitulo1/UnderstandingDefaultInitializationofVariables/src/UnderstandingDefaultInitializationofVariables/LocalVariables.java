06f8f8ecac146a937b7d6d384f9f750366840895
warriorskil18 <warriorskil18@gmail.com>
2026-05-12 13:21:04 -0600

    Termino Declaring and Initializing Variables

--- Capitulo1/UnderstandingDefaultInitializationofVariables/src/UnderstandingDefaultInitializationofVariables/LocalVariables.java
+++ Capitulo1/UnderstandingDefaultInitializationofVariables/src/UnderstandingDefaultInitializationofVariables/LocalVariables.java
@@ -43,6 +43,25 @@ public class LocalVariables {
 			return reply;
 			
 			
-			//76
-	}
+			/*
+			 * El compilador tambien es lo suficientemente inteligente para reconocer inicializaciones mas complejas
+			 * 
+			 */
+		}
+			
+			public void findAnswer(boolean check) {
+				int answer;
+				int onlyOneBranch;
+				if(check) {
+					onlyOneBranch = 1;
+					answer = 1;
+					}else {
+						answer = 2;
+					}
+				System.out.println(answer);
+			//	System.out.println(onlyOneBranch); // No compila
+			}
+			
+			
+	
 }
